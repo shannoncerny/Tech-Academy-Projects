242701269887bedf777e89ba14c54ea48d90a98c
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-11 14:35:38 -0600

    Created website with music video embedded

---
+++ FavoriteSong/FavoriteSong/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+
+function newColor() {
+    document.getElementById("title_band").style.color = "white";
+}
+
+function oldColor() {
+    document.getElementById("title_band").style.color = "black";
+}
