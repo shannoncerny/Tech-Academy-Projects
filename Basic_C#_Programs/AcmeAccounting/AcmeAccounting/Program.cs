0bd40b4b344ea373ce0581f3e0e9822ff9c8dd9f
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-14 14:28:45 -0500

    created Acme Accounting Systems assingment

---
+++ Basic_C#_Programs/AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems\n" +
+            "Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
+
